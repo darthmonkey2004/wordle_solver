afcabb18abbfe3e78fda9e8fcf8018c7107eea4e
darthmonkey2004 <darthmonkey2004@users.noreply.github.com>
2022-09-30 06:43:24 -0500

    Added sdist line to install.sh to install without tarball...

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ haspip=$(which pip3)
 if [ -z "$haspip" ]; then
 	sudo apt-get install -y python3-pip3
 fi
+python3 setup.py sdist
 pip3 install --user "dist/wordle_helper-1.0.tar.gz" -r requirements.txt
