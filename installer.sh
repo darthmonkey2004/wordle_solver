43ab0767edf191700ff2b7bb5f0d32890d7b25a4
Matt McClellan <darthmonkey2004@gmail.com>
2023-02-27 13:19:41 -0600

    corrected missing dependencies with pip (used apt-get for chromedriver/selenium)

--- installer.sh
+++ installer.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
-is_installed=$(which wordle)
-if [ -n "$is_installed" ]; then
-	echo "Already appears to be installed! Aborting setup..."
-	exit 0
-fi
-cd ~/
-haspip=$(which pip3)
-if [ -z "$haspip" ]; then
-	echo "PyPi not installed! Installing..."
-	sudo apt-get install -y python3-pip3
-fi
-hasgit=$(which git)
-if [ -z "$hasgit" ]; then
-	echo "GIT cli not installed! Installing..."
-	sudo apt-get install -y git
-fi
-echo "Downloading from git repo..."
-git clone "https://darthmonkey2004/wordle_helper.git"
-cd wordle_helper
 echo "Installing..."
-pip3 install --user "dist/wordle_helper-1.0.tar.gz" -r requirements.txt
+sudo apt-get install -y chromium-bsu chromium-chromedriver python3-selenium 
+dir=$(pwd)
+tarfile=$(find "$dir" -name "*wordle*" | grep "tar.gz")
+echo "installing tarball: '$tarfile'..."
+pip3 install --user "$tarfile" -r requirements.txt
+echo "Checking bashrc.."
 rcpatched=$(cat "$HOME/.bashrc" | grep "/.local/bin")
 if [ -z "$rcpatched" ]; then
 	echo "Patching bashrc to add .local/bin to path..."
